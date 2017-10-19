655f94b0d168264a0d9a38550ec81a29b212b7be
biek <6103119+biek@users.noreply.github.com>
2017-10-19 11:42:55 +0200

    Update colour_value.py

--- colour_value.py
+++ colour_value.py
@@ -1,3 +1,5 @@
+# defines colour values
+
 def colour_values(red,yellow, green, brown, blue, pink, black):
     print "Red:%r\nYellow:%r\nGreen:%r\nBrown:%r\nBlue:%r\nPink:%r\nBlack%r" % (
         red,yellow,green,brown,blue,pink,black)
