158c5c1dca690f6b13776de1e84c330fac46dcf1
biek <6103119+biek@users.noreply.github.com>
2017-10-19 11:42:33 +0200

    Create colour_value.py

---
+++ colour_value.py
@@ -0,0 +1,5 @@
+def colour_values(red,yellow, green, brown, blue, pink, black):
+    print "Red:%r\nYellow:%r\nGreen:%r\nBrown:%r\nBlue:%r\nPink:%r\nBlack%r" % (
+        red,yellow,green,brown,blue,pink,black)
+        
+colour_values("1","2","3","4","5","6","7")
